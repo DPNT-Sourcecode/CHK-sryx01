73a1b5a6b5325dacad4a0b45dd02ff29f8f3915b
sbx_user1051 <sbx_user1051@169.254.53.165>
2024-03-24 22:26:33 +0000

    Sun Mar 24 22:26:33 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,40 +1,42 @@
 package befaster.solutions.CHK;
 
+import com.sun.corba.se.impl.naming.cosnaming.BindingIteratorImpl;
+
 import java.util.HashMap;
 
-//+------+-------+----------------+
-//| Item | Price | Special offers |
-//+------+-------+----------------+
-//| A    | 50    | 3A for 130     |
-//| B    | 30    | 2B for 45      |
-//| C    | 20    |                |
-//| D    | 15    |                |
-//+------+-------+----------------+
+//+------+-------+------------------------+
+//| Item | Price | Special offers         |
+//+------+-------+------------------------+
+//| A    | 50    | 3A for 130, 5A for 200 |
+//| B    | 30    | 2B for 45              |
+//| C    | 20    |                        |
+//| D    | 15    |                        |
+//| E    | 40    | 2E get one B free      |
+//+------+-------+------------------------+
 public class CheckoutSolution {
+    private Integer checkoutBasketSum = 0;
 
-    public Integer checkout(String skus) {
-        Integer checkoutBasketSum = 0;
-        Integer counterA = 0;
-        Integer counterB = 0;
-        HashMap<Character, Integer> prices = new HashMap<>();
+    private static HashMap<Character, Integer> prices = new HashMap<>();
+    private HashMap<Character, Integer> productCount  = new HashMap<>();
 
+    private static void initProducts(){
         prices.put('A',50);
         prices.put('B',30);
         prices.put('C',20);
         prices.put('D',15);
+    }
+
+    public Integer checkout(String skus) {
+        initProducts();
 
         for (char c : skus.toCharArray()){
             if (!prices.containsKey(c)) {
                 return -1;
             }
+            else {
+                productCount.put(c, productCount.get(c) + 1);
+            }
             checkoutBasketSum += prices.get(c);
-
-            if (c == 'A') {
-                counterA++;
-            }
-            else if (c == 'B') {
-                counterB++;
-            }
         }
 
         checkoutBasketSum -= (counterA/3 * 20);
@@ -42,3 +44,4 @@ public class CheckoutSolution {
         return checkoutBasketSum;
     }
 }
+
